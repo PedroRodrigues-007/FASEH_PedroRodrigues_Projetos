8c916f2322a307d9d451caacba76326137f43765
PedroRodrigues-007 <pedrook654@gmail.com>
2026-04-07 21:11:55 -0300

    joia

---
+++ aulas-java/atividade.idea/atividade_07-04/scratch.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Scratch {
+    public static void main(String[] args) {
+        String nomeVendedor;
+        float salarioFixo, valorVendas, comissao, salarioTotal;
+
+        Scanner receberDados = new Scanner(System.in);
+
+        System.out.println("Digite o nome do vendedor:");
+        nomeVendedor = receberDados.next();
+
+        System.out.println("Digite o salário fixo do vendedor:");
+        salarioFixo = Float.parseFloat(receberDados.next());
+
+        System.out.println("Digite o valor das vendas:");
+        valorVendas = Float.parseFloat(receberDados.next());
+
+        comissao = 0.15f;
+
+        salarioTotal = salarioFixo + (valorVendas * comissao);
+
+        System.out.println("O salário total do vendedor com comissão é: " + salarioTotal);
+
+        receberDados.close();
+    }
+}
